3ffd3f87dae22b407d0089746e5e67fe0a66d4ca
Gleison Amorim <54336609+GleisonAmorim@users.noreply.github.com>
2024-01-03 11:05:37 -0300

    Delete Ultima School -Desenvolvedo r Python completo/Módulo 2 SQL e banco de dados/1ª semana directory

--- Ultima School -Desenvolvedo r Python completo/Módulo 2 SQL e banco de dados/1ª semana/insercao.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-conexao = sqlite3.connect('banco.sqlite3')
-cursor = conexao.cursor()
-sql = '''
-insert into categoria (descricao) values('celulares')
-'''
-cursor.execute(sql)
-conexao.commit()
-conexao.close()
